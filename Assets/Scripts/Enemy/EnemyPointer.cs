e3c65b2c139fac0f49970a5c6a712dabd6d7ad68
Leo7416 <adolfhiko@gmail.com>
2024-07-27 14:02:17 +0300

    add healthbar and marker on enemy

---
+++ Assets/Scripts/Enemy/EnemyPointer.cs
@@ -0,0 +1,21 @@
+﻿using SampleArcade.GameManagers;
+using System;
+using UnityEngine;
+
+namespace SampleArcade.Enemy
+{
+    public class EnemyPointer : MonoBehaviour
+    {
+        public event Action<EnemyPointer> OnDestroyed;
+
+        private void Start()
+        {
+            PointerManager.Instance.AddToList(this);
+        }
+
+        private void OnDestroy()
+        {
+            PointerManager.Instance.RemoveFromList(this);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/HeathBarUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SampleArcade.UI
+{
+    public class HeathBarUI : MonoBehaviour
+    {
+        [SerializeField]
+        private Slider _healthSlider;
+        private Transform _cameraTransform;
+
+        private void Awake()
+        {
+            _healthSlider = GetComponent<Slider>();
+            _cameraTransform = UnityEngine.Camera.main.transform;
+        }
+
+        private void LateUpdate()
+        {
+            // Always face the camera
+            transform.LookAt(transform.position + _cameraTransform.rotation * Vector3.forward, _cameraTransform.rotation * Vector3.up);
+        }
+        public void SetHealth(float healthPercent)
+        {
+            _healthSlider.value = healthPercent;
+        }
+    }
+}
